604e01f251992ac92fd62b746b63c7a1430fce89
eltchi <elsa.tchissambo@gmail.com>
2025-09-27 17:56:49 +0200

    install bootstrap and add responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
 import React from "react";
-import {Routes, Route, Link} from "react-router-dom";
+//import {Routes, Route, Link} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
-import Home from './pages/Home';
-import Service from './pages/Service';
-import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
-import Legal from './pages/Legal';
+//import Home from './pages/Home';
+//import Service from './pages/Service';
+//import Portfolio from './pages/Portfolio';
+//import Contact from './pages/Contact';
+//import Legal from './pages/Legal';
+import Navigationbar from './Navigationbar';
 
 
 export default function App() {
   return (
     <div className="App">
-      <nav>
-        <Link to="/">Accueil</Link>
-        <Link to="/service">Services</Link>
-        <Link to="/portfolio">Portfolio</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/legal">Mentions légales</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/service" element={<Service/>}></Route>
-        <Route path="/portfolio" element={<Portfolio/>}></Route>
-        <Route path="/contact" element={<Contact/>}></Route>
-        <Route path="/legal" element={<Legal/>}></Route>
-      </Routes>
+      <Navigationbar/>
     </div>
   )
 }
---
+++ src/Navigationbar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Routes, Route, Link } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Service from './pages/Service';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Legal from './pages/Legal';
+
+
+function Navigationbar() {
+  return (
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary" fixed="top">
+        <Container>
+          <Navbar.Brand as={Link} to="/">Jeanne Doe</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/">Accueil</Nav.Link>
+              <Nav.Link as={Link} to="/service">Services</Nav.Link>
+              <Nav.Link as={Link} to="/portfolio">Portfolio</Nav.Link>
+              <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+              <Nav.Link as={Link} to="/legal">Mentions légales</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {/* wrapper pour gérer le décalage dû à fixed="top" */}
+      <div className="page-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/legal" element={<Legal />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default Navigationbar;
